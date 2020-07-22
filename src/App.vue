65fb50f67e04561d06cb1fad44fd68d66e5711dc
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-07-22 20:18:01 +0100

    Commit 1(1.0)

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <QuickThinkHead></QuickThinkHead>
+    <instructions></instructions>
+    <Customize></Customize>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    greet: () => {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
