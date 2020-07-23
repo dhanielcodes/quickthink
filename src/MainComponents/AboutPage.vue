c4745b41da184b6836af852b64e4d16e459f9db0
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-07-23 02:38:23 +0100

    About

--- src/MainComponents/AboutPage.vue
+++ src/MainComponents/AboutPage.vue
@@ -1,7 +1,34 @@
 <template>
-<div>
+<main>
     <h1>About</h1>
-</div>
+    <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eros mauris cum risus semper est 
+       facilisi in. Dolor feugiat adipiscing phasellus viverra bibendum tincidunt urna eu ac.
+        Quam eu nulla egestas habitasse aliquam, amet, id neque, suscipit. Aliquet nisi, eu sed viverra leo. 
+        Pharetra quisque in ultrices magna hac amet, amet ullamcorper quis. Quis pretium, sit tempus, imperdiet. 
+        Eget euismod sed urna purus pellentesque mauris varius vel adipiscing. Eget sit viverra vel donec 
+        curabitur fusce vitae eu vestibulum. Rutrum metus, at in sed lectus commodo dictum semper id. Neque 
+        suspendisse porttitor id nunc euismod. Tempus vitae aliquam blandit purus velit tortor, sem justo. Morbi 
+        fames ultricies gravida velit, congue odio integer id gravida. Tellus viverra amet, at eleifend posuere 
+        feugiat at iaculis sed. Venenatis volutpat a vel neque. Sit nisl feugiat imperdiet sollicitudin egestas. 
+        Egestas scelerisque arcu eu, id sed hendrerit. Nibh natoque id vitae leo leo nunc, egestas nulla viverra. 
+        Fusce euismod sit mattis mi at. Dui, eget turpis scelerisque viverra tincidunt proin est est mi. Ullamcorper 
+        tristique rhoncus nisi, sagittis vel aliquam dui ut elementum. Tellus donec vel vel pulvinar mauris 
+        scelerisque tincidunt ultricies semper. Facilisis tellus sed eu viverra lorem nec venenatis ipsum in. 
+        Laoreet porttitor netus leo, vestibulum lorem nunc, morbi pharetra ut. Laoreet hendrerit pretium tempus 
+        porttitor iaculis. Nulla at eu, cursus odio morbi mattis ornare neque gravida. Fermentum, nec gravida 
+        convallis augue facilisi vel at. A ullamcorper rutrum luctus enim enim vel quis. Pellentesque varius 
+        pellentesque eu at erat ac neque. Non convallis nisl in amet. In dui est ipsum nibh eget blandit euismod 
+        commodo. Quis fermentum, est, a nibh fermentum scelerisque a quam a. Condimentum ut morbi tristique lectus 
+        tempus ac. Lorem eu, nec mus nunc amet. Turpis malesuada rutrum sagittis tempor enim ut. Tincidunt ac tortor 
+        nam enim aenean donec massa urna arcu. Elit adipiscing penatibus pharetra orci, sit fermentum ullamcorper. 
+        Eu augue fermentum faucibus faucibus sed duis pellentesque eros, bibendum. Pulvinar nunc, nam aliquet quisque. 
+        Egestas aliquam lectus consequat varius tellus, imperdiet urna curabitur faucibus. Dictum nibh ullamcorper purus 
+        quam proin sapien magna. Etiam nullam adipiscing morbi lobortis diam elit viverra. Sit volutpat porttitor duis 
+        odio laoreet aliquet fringilla vitae. Lectus cras sed cursus nunc eget augue mattis donec non. Pellentesque vitae 
+        sagittis auctor ipsum et, lorem pharetra pretium.
+    </p>
+</main>
 </template>
 
 <script>
@@ -19,7 +46,21 @@ export default {
 </script>
 
 <style scoped>
-div{
-  height: 100vh;
+main{
+  height: auto;
+  font-family: 'DM sans';
+  text-align: center;
+}
+h1{
+  width: 100%;
+  font-size: 60px;
+  padding: 110px 0;
+  text-align: center;
+  background: #1C1046;
+  color: white;
+}
+p{
+  font-size: 20px;
+  padding: 50px;
 }
 </style>
