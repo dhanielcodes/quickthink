6afc3448fa71a23c514433f705e42b6b32cb8a49
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-07-23 19:54:20 +0100

    Implementations of login and signup page

--- src/MainComponents/SignupPage.vue
+++ src/MainComponents/SignupPage.vue
@@ -1,6 +1,14 @@
 <template>
 <div>
-    <h1>Signup</h1>
+    <form action="">
+       <h1>Hey There!</h1>
+       <p>Sign up to play cool games</p>
+       <input type="name" placeholder="Name">
+       <input type="email" placeholder="Email">
+       <input type="password" placeholder="Password">
+       <button>Create Account</button>
+       <p>Already have and account? <router-link to="/login"><a href="">Login</a></router-link> </p>
+    </form>
 </div>
 </template>
 
@@ -21,5 +29,51 @@ export default {
 <style scoped>
 div{
   height: 100vh;
+  font-family: 'DM sans';
+  position: relative;
+  background: #1C1046;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.253);
+}
+form{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  text-align: center;
+  padding: 50px;
+  border-radius: 8px;
+  background: white;
+}
+form input{
+  width: 100%;
+  padding: 20px;
+  border-radius: 8px;
+  border: 0.5px solid #1c104657;
+  margin: 20px 0;
+  font-size: 17px;
+  font-family: 'DM sans';
+  outline: none;
+}
+::placeholder{
+  font-size: 17px;
+  font-family: 'Dm sans';
+}
+form button{
+  width: 100%;
+  padding: 20px;
+  border-radius: 8px;
+  border: 0px;
+  color: white;
+  margin: 20px 0;
+  background: #18C5D9;
+  font-size: 17px;
+  font-family: 'DM sans';
+  outline: none;
+  cursor: pointer;
+}
+a{
+  color: #18C5D9;
+  font-weight: 600;
 }
 </style>
