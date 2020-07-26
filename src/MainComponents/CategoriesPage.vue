57f887fdc91ad467424252b0d5b3d85b6fa86dcf
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-07-26 17:19:23 +0100

    SHine

--- src/MainComponents/CategoriesPage.vue
+++ src/MainComponents/CategoriesPage.vue
@@ -1,9 +1,9 @@
 <template>
 
     <div class="categories">
-      <header>
-        <span><h1 id="gamee">Game Categories</h1></span>
-      </header>
+
+            <div class="shine"><h1>Categories</h1></div>
+
       
       
 
@@ -129,6 +129,77 @@ export default {
 </script>
 
   <style scoped>
+
+
+
+
+.shine {
+  padding: 100px  0;
+  width:100%;
+  background:#1C1046;
+  z-index:0;
+  position: relative;
+   overflow: hidden;
+   margin-bottom: 50px;
+   font-family: 'DM sans';
+}
+
+/* text */
+.shine h1 {
+  color:#fff;
+  left:50%;
+  transform:translate(-50%,-50%);
+  position: absolute;
+  display: inline-block;
+  font-family:'open sans',sans-serif;
+  letter-spacing:-1px;
+  font-weight:bold;
+  top:50%;
+  width: 100%;
+  font-size: 50px;
+  text-align: center;
+}
+
+/* Shine */
+.shine::after {
+	content:'';
+  top:0;
+	transform:translateX(100%);
+	width:100%;
+	height:220px;
+	position: absolute;
+	z-index:1;
+	animation: slide 1s infinite;
+	 
+  /* 
+  CSS Gradient - complete browser support from http://www.colorzilla.com/gradient-editor/ 
+  */
+  background: -moz-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(128,186,232,0) 99%, rgba(125,185,232,0) 100%); /* FF3.6+ */
+	background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(255,255,255,0)), color-stop(50%,rgba(255,255,255,0.8)), color-stop(99%,rgba(128,186,232,0)), color-stop(100%,rgba(125,185,232,0))); /* Chrome,Safari4+ */
+	background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Chrome10+,Safari5.1+ */
+	background: -o-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Opera 11.10+ */
+	background: -ms-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* IE10+ */
+	background: linear-gradient(to right, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* W3C */
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#007db9e8',GradientType=1 ); /* IE6-9 */
+}
+
+/* animation */
+
+@keyframes slide {
+	0% {transform:translateX(-100%);}
+	100% {transform:translateX(100%);}
+}
+
+
+
+
+
+
+
+
+
+
+
   header h1{
   animation:aa  2s 0s 1 forwards;
   }
@@ -307,7 +378,10 @@ export default {
   transform: rotate(-4deg);
 }
   @media only screen and (max-width: 550px){
-    header h1 {
+     .shine{
+    padding: 60px;
+  }
+    .shine h1 {
       font-size: 30px ;
     }
 
