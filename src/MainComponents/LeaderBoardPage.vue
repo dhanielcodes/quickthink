5e80f8dd6bc8255eac285cda9f46178c00f8bfb1
p_yn3 <66219025+payn33@users.noreply.github.com>
2020-07-27 19:20:13 +0100

    Update LeaderBoardPage.vue

--- src/MainComponents/LeaderBoardPage.vue
+++ src/MainComponents/LeaderBoardPage.vue
@@ -27,7 +27,7 @@
     <div class="board-data" >
       <button class="l-dbtn" @click='fetchBoardData' v-on:click="btn = !btn, li = !li" v-bind:class="{fade: btn}">Show leader Board</button>
         <ul id="v-for-board" v-bind:class="{fades: li}">
-          <h1 v-if="loading">Loading...</h1>
+          <h2 v-if="loading">Loading...</h2>
           <li v-for="results in board" class="li-anim">
             <p class="id"> {{results.id}} .</p><img src="../assets/images/leaderimage.svg" alt=""><p class="name">{{results.user_name}}</p><p class="geo">{{results.game_code}}</p><p class="score">{{results.score}}</p>
           </li>
@@ -106,32 +106,32 @@ export default {
 
 /* Shine */
 .shine::after {
-	content:'';
+  content:'';
   top:0;
-	transform:translateX(100%);
-	width:100%;
-	height:220px;
-	position: absolute;
-	z-index:1;
-	animation: slide 1s infinite;
-	 
+  transform:translateX(100%);
+  width:100%;
+  height:220px;
+  position: absolute;
+  z-index:1;
+  animation: slide 1s infinite;
+   
   /* 
   CSS Gradient - complete browser support from http://www.colorzilla.com/gradient-editor/ 
   */
   background: -moz-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(128,186,232,0) 99%, rgba(125,185,232,0) 100%); /* FF3.6+ */
-	background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(255,255,255,0)), color-stop(50%,rgba(255,255,255,0.8)), color-stop(99%,rgba(128,186,232,0)), color-stop(100%,rgba(125,185,232,0))); /* Chrome,Safari4+ */
-	background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Chrome10+,Safari5.1+ */
-	background: -o-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Opera 11.10+ */
-	background: -ms-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* IE10+ */
-	background: linear-gradient(to right, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* W3C */
-	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#007db9e8',GradientType=1 ); /* IE6-9 */
+  background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(255,255,255,0)), color-stop(50%,rgba(255,255,255,0.8)), color-stop(99%,rgba(128,186,232,0)), color-stop(100%,rgba(125,185,232,0))); /* Chrome,Safari4+ */
+  background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Chrome10+,Safari5.1+ */
+  background: -o-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* Opera 11.10+ */
+  background: -ms-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* IE10+ */
+  background: linear-gradient(to right, rgba(255,255,255,0) 0%,rgba(255,255,255,0.8) 50%,rgba(128,186,232,0) 99%,rgba(125,185,232,0) 100%); /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#007db9e8',GradientType=1 ); /* IE6-9 */
 }
 
 /* animation */
 
 @keyframes slide {
-	0% {transform:translateX(-100%);}
-	100% {transform:translateX(100%);}
+  0% {transform:translateX(-100%);}
+  100% {transform:translateX(100%);}
 }
 
 
@@ -147,12 +147,18 @@ export default {
   place-items: center;
   color: black;
 }
-
+.board-data h2{
+  font-size: 40px;
+  color: #fff;
+}
 
 ul {
   text-align: center;
 }
 
+ColorZilla
+Ultimate CSS Gradient Generator from ColorZilla
+A powerful Photoshop-like CSS gradient editor
 p {
   margin-right: 5px;
 }
@@ -219,14 +225,14 @@ h1{
 }
 
 .h1-div:after {
-	content:'';
+  content:'';
   top:0;
-	transform:translateX(100%);
-	width:100%;
-	height:220px;
-	position: absolute;
-	z-index:1;
-	animation: slide 1s infinite;
+  transform:translateX(100%);
+  width:100%;
+  height:220px;
+  position: absolute;
+  z-index:1;
+  animation: slide 1s infinite;
 }
 
 .trophies div {
